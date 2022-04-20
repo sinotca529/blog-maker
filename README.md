c2d7d86e7c5521c8962d5b242399c930c13dc9bf
sinotca529-wsl <sinotca35963@gmail.com>
2022-04-20 10:54:16 +0900

    update template: add feature to optout viz.js, mermaid.js, pseudocode.js

--- README.md
+++ README.md
@@ -35,4 +35,14 @@ date: yyyy-mm-dd で日付を指定する
 `blog.py`を叩くと、ブログ用のフォルダにサブディレクトリ`output`が作成されます。
 その中にある`index.html`(自動生成されます)がブログのトップページとなります。
 
-
+# プラグインの有効化
+YAMLヘッダを次のように書くことで、便利なプラグイン(js)が有効化されます。
+```yaml
+plug:
+    # コードブロックでgraphvizが使えるようになります (言語名にgraphvizを指定)
+    graphviz: true
+    # コードブロックでlatex記法の擬似コードが使えるようになります (言語名にalgorithmを指定)
+    pseudocode: true
+    # コードブロックでmermaidが使えるようになります (言語名にmermaidを指定)
+    mermaid: true
+```
--- script.js
+++ script.js
@@ -6,16 +6,4 @@ window.addEventListener("DOMContentLoaded", function() {
             let match = re.exec(a.href);
             a.href = match[1] + encodeURIComponent(match[2])
         });
-
-    // pseudo.js
-    document.querySelectorAll(".algorithm")
-        .forEach(e => {
-            pseudocode.renderElement(e, {lineNumber: true, lineNumberPunc: ' '});
-        });
-
-    // viz.js (graphviz)
-    document.querySelectorAll(".graphviz")
-        .forEach(e => {
-            e.innerHTML = Viz(e.innerText);
-        });
 });
